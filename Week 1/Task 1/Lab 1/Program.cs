1bca2755f764e1c14cc62a642e0c7de11190a4fb
Aidana Abdrasilova <meredit.manson.00@gmail.com>
2019-02-13 02:41:11 +0600

    first commit

---
+++ Week 1/Task 1/Lab 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static bool IsPrime(int number) // Bool function which returns true or false
+        {
+            if (number == 1 || number == 0) return false;// If number equal to 1 or 0 it will return false
+            for (int k = 2; k < number; ++k) // From 2 to number
+            {
+                if (number % k == 0) return false; // Returns false if number by dividing by the remainder is egual to 0
+            }
+            return true; // Returns true if the above conditions does not match
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // Variable n which is the amount of numbers in array
+            string s = Console.ReadLine();
+            string[] a = s.Split(); // An array in which every element is the number from s devided by space
+            int[] array = new int[n]; // An array with n numbers
+            for (int k = 0; k < n; ++k)
+            {
+                array[k] = int.Parse(a[k]); // Record every value from a into int to array
+            }
+            int cnt = 0; // Calculates prime numbers
+            for (int k = 0; k < n; ++k)
+            {
+                if (IsPrime(array[k]) == true) // Condition which is performed if the function  returns true 
+                {
+                    cnt++; // Counting prime numbers
+                }
+            }
+            Console.WriteLine(cnt); // Amount of prime numbers
+            for (int k = 0; k < n; ++k)
+            {
+                if (IsPrime(array[k]) == true) // Function returns true if number is prime
+                    Console.Write(array[k] + " "); // Show prime numbers
+            }
+            Console.ReadKey(); // Works until press any key
+        }
+
+    }
+}
---
+++ Week 1/Task 2/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string Id;
+        public int YeartOFStudy;
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.Id = id;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetId()
+        {
+            return Id;
+        }
+        public int GetYearOfStudy()
+        {
+            return ++YeartOFStudy;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Info = new Student(Console.ReadLine(), Console.ReadLine());
+            Console.WriteLine(Info.GetName());
+            Console.WriteLine(Info.GetId());
+            Console.WriteLine(Info.GetYearOfStudy());
+            Console.ReadKey();
+        }
+    }
+}
